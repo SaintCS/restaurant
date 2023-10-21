3db12da1ecda68efef1747be979c03e923c5f162
Saint <148532009+SaintCS@users.noreply.github.com>
2023-10-21 19:36:22 +0200

    Finish menu + take in order. Added receipt

--- restaurant.py
+++ restaurant.py
@@ -1,89 +1,72 @@
+# imports
 import os
 import random
 
-# Function to clear terminal
-def clear_terminal():
-    os.system('cls||clear')
+# Wipe terminal on every start
+os.system('cls' if os.name == 'nt' else 'clear')
 
-# Menu items, prices, and codes
-menu = {
-    1: {'name': 'French Fries', 'price': 2.00},
-    2: {'name': '1/4 Pound Burger', 'price': 5.00},
-    3: {'name': '1/4 Pound Cheese Burger', 'price': 5.55},
-    4: {'name': '1/2 Pound Burger', 'price': 7.00},
-    5: {'name': '1/2 Pound Cheeseburger', 'price': 7.50},
-    6: {'name': 'Medium Pizza', 'price': 9.00},
-    7: {'name': 'Medium Pizza + Extra Toppings', 'price': 11.00},
-    8: {'name': 'Large Pizza', 'price': 12.00},
-    9: {'name': 'Large Pizza + Extra Toppings', 'price': 14.50},
-    10: {'name': 'Garlic Bread', 'price': 4.50}
-}
+# Lists
+menuItems = ['French Fries', '1/4 Pound Burger', '1/4 Pound Cheeseburger', '1/2 Pound Burger', '1/2 Pound Cheeseburger', 'Medium Pizza', 'Medium Pizza + Extra Toppings', 'Large Pizza', 'Large Pizza + Extra toppings', 'Garlic bread']
+menuPrices = [2.00, 5.00, 5.55, 7.00, 7.50, 9.00, 11.00, 12.00, 14.50, 4.50]
+menuCodes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-# Function to print menu
-def print_menu():
-    print('MENU')    
-    print('---------------------------------------------------------')
-    print('Code\tItem\t\t\t\t\tPrice')
-    print('---------------------------------------------------------')
-    for code, item in menu.items():
-        print(f'{code}\t{item["name"]:<30}\t\t${item["price"]:.2f}')
-    print('---------------------------------------------------------')
+# Show menu
+def displayMenu():
+    print('\t\tMENU')
+    print('------------------------------------------------------------------')
+    print('Code\t\tItem\t\t\t\t\tPrice')
+    print('------------------------------------------------------------------')
+    for menu in range(len(menuItems)):
+        print(f'{menuCodes[menu]}\t\t{menuItems[menu]:<30}\t\t${menuPrices[menu]:.2f}')
+    print('------------------------------------------------------------------')
+
+displayMenu()
 
 
-# Initialize variables
+
+orderItems = []
+orderAmount = []
+orderPrice = []
+
+zippedlist = zip(orderItems, orderAmount)
+
+# Get user input
 def get_order():
-    order = []
-    total_price = 0
 
-    # Ask for order
+
     while True:
-        # Ask for item number
-        item_num = int(input('Enter the item number you want to order: '))
-        while item_num not in menu:
-            item_num = int(input('Invalid item number. Please enter a valid item number: '))
+            activeOrder = int(input('Please put in the code of the item you want to order (to end type 10): '))
 
-        # Ask for quantity
-        quantity = int(input('Enter the quantity you want to order: '))
-        while quantity < 1:
-            quantity = int(input('Invalid quantity. Please enter a valid quantity: '))
+            if activeOrder == 10:
+                break
 
-        # Add item to order list
-        order.append((item_num, quantity))
+            orderItems.append(menuItems[activeOrder])
 
-        # Ask if they want to order anything else
-        more = input('Do you want to order anything else? (y/n): ')
-        while more.lower() not in ['y', 'n']:
-            more = input('Invalid input. Please enter y or n: ')
-        if more.lower() == 'n':
-            break
+            if activeOrder == 10:
+                print('invalid code')
+            else:
+                print('You have chosen: ', menuItems[activeOrder])
+                amount = int(input('How much of this item? '))
+                orderAmount.append(amount)
+                orderPrice.append(amount * menuPrices[activeOrder])
+            
+get_order()
 
-    # Calculate total price
-    for item in order:
-        item_num = item[0]
-        quantity = item[1]
-        total_price += menu[item_num]['price'] * quantity
-
-    return order, total_price
+# Clear terminal to display receipt cleanly
+os.system('cls' if os.name == 'nt' else 'clear')
 
 # Print receipt
-def print_receipt(order, total_price):
-    clear_terminal()
-    print('Items Ordered\t\t\t\t\tPrice')
-    print('---------------------------------------------------------')
-    for item in order:
-        item_num = item[0]
-        quantity = item[1]
-        print(f'{quantity} {menu[item_num]["name"]:<30}\t\t${menu[item_num]["price"] * quantity:.2f}')
-    print('---------------------------------------------------------')
-    print(f'Total\t\t\t\t\t\t${total_price:.2f}')
-    print(f'Order Number:\t\t\t\t\t{random.randint(1000, 9999)}')
-    print('---------------------------------------------------------')
+def receipt():
+    numReceipt = random.randint(1695,40501)
+    print(f'\t\t\tRECEIPT | #{numReceipt}')
+    print('------------------------------------------------------------------')
+    print('Items\t\t\t\tAmount')
+    print('------------------------------------------------------------------')
+    for allitems in zip(orderItems, orderAmount):
+        print(f'{orderItems}\t\t{orderAmount}\t\t')
+    print('------------------------------------------------------------------')
+    print(f'The total price for your order is: ${sum(orderPrice)}')
 
-# Print menu
-print_menu()
 
-# Get order
-order, total_price = get_order()
 
-# Print receipt
-print_receipt(order, total_price)
+receipt()
